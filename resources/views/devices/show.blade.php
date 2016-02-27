9851f02cbeedfda097a9306118c1b4a6d6e8fa7c
Luke Tanprasit <tanprasitp@hotmail.co.uk>
2016-02-27 16:57:53 +0000

    Updated device views to accommodate for alerts.

--- resources/views/devices/show.blade.php
+++ resources/views/devices/show.blade.php
@@ -5,43 +5,41 @@
 @stop
 
 @section('content')
-<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-    <h1 class="page-header">Device Details</h1>
-    <div class="col-lg-6">
-            <div class="form-group col-lg-12">
-                <label>Model</label>
-                <input type="text" name="model" class="form-control" value="{{ $device->model }}" readonly="readonly">
-            </div>
+<h1 class="page-header">Device Details</h1>
+<div class="col-lg-6">
+        <div class="form-group col-lg-12">
+            <label>Model</label>
+            <input type="text" name="model" class="form-control" value="{{ $device->model }}" readonly="readonly">
+        </div>
 
-            <div class="form-group col-lg-12">
-                <label>Manufactorer</label>
-                <input type="text" name="manufactorer" class="form-control" value="{{ $device->manufactorer }}" readonly="readonly">
-            </div>
+        <div class="form-group col-lg-12">
+            <label>Manufactorer</label>
+            <input type="text" name="manufactorer" class="form-control" value="{{ $device->manufactorer }}" readonly="readonly">
+        </div>
 
-            <div class="form-group col-lg-12">
-                <label>Product</label>
-                <input type="text" name="product" class="form-control" value="{{ $device->product }}" readonly="readonly">
-            </div>
+        <div class="form-group col-lg-12">
+            <label>Product</label>
+            <input type="text" name="product" class="form-control" value="{{ $device->product }}" readonly="readonly">
+        </div>
 
-            <div class="form-group col-lg-12">
-                <label>SDK Version</label>
-                <input type="text" name="sdk_version" class="form-control" value="{{ $device->sdk_version }}" readonly="readonly">
-            </div>
+        <div class="form-group col-lg-12">
+            <label>SDK Version</label>
+            <input type="text" name="sdk_version" class="form-control" value="{{ $device->sdk_version }}" readonly="readonly">
+        </div>
 
-            <div class="form-group col-lg-12">
-                <label>Serial Number</label>
-                <input type="text" name="serial_number" class="form-control" value="{{ $device->serial_number }}" readonly="readonly">
-            </div>
-    </div>
-    <div class="col-lg-6">
-        <h2>Terms and Conditions</h2>
-        <p>By clicking on "Register" you agree to The Company's' Terms and Conditions</p>
+        <div class="form-group col-lg-12">
+            <label>Serial Number</label>
+            <input type="text" name="serial_number" class="form-control" value="{{ $device->serial_number }}" readonly="readonly">
+        </div>
+</div>
+<div class="col-lg-6">
+    <h2>Terms and Conditions</h2>
+    <p>By clicking on "Register" you agree to The Company's' Terms and Conditions</p>
 
-       <p>While rare, prices are subject to change based on exchange rate fluctuations - should such a fluctuation happen, we may request an additional payment. You have the option to request a full refund or to pay the new price. (Paragraph 13.5.8)</p>
+   <p>While rare, prices are subject to change based on exchange rate fluctuations - should such a fluctuation happen, we may request an additional payment. You have the option to request a full refund or to pay the new price. (Paragraph 13.5.8)</p>
 
-        <p>Should there be an error in the description or pricing of a product, we will provide you with a full refund (Paragraph 13.5.6)</p>
+    <p>Should there be an error in the description or pricing of a product, we will provide you with a full refund (Paragraph 13.5.6)</p>
 
-        <p>Acceptance of an order by us is dependent on our suppliers ability to provide the product. (Paragraph 13.5.6)</p>
-    </div>
+    <p>Acceptance of an order by us is dependent on our suppliers ability to provide the product. (Paragraph 13.5.6)</p>
 </div>
 @stop
